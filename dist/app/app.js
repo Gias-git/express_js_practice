da5490d5d30512e17ec598cec865e96f64664a96
Gias-git <giasuddin.programmer@gmail.com>
2025-06-28 19:04:13 +0600

    Global error handling

--- dist/app/app.js
+++ dist/app/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -10,7 +19,24 @@ const todos_routes_1 = require("./todos/todos.routes");
 app.use(express_1.default.json());
 app.use("/todos", todos_routes_1.todosRouter);
 const filepath = path_1.default.join(__dirname, "../../Db/todo.json");
-app.get('/', (req, res) => {
-    res.send('WELCOME TO TODOS APPasas');
+app.get('/', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        console.log("something");
+        res.send("WELCOME TO TODOS");
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+//route not found error {last of the all routes }
+app.use((req, res, next) => {
+    res.status(400).json({ message: "Route not find" });
+});
+// global error handling - it's must be last of the all routes
+app.use((error, req, res, next) => {
+    if (error) {
+        console.log("something ERROR");
+        res.status(500).json({ message: "Something wrong", error });
+    }
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
 const app: Application = express()
 import fs from "fs"
 import path from 'path'
@@ -12,8 +12,34 @@ app.use("/todos", todosRouter)
 
 const filepath = path.join(__dirname, "../../Db/todo.json")
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('WELCOME TO TODOS APPasas')
+app.get('/',
+
+
+    async (req: Request, res: Response, next: NextFunction) => {
+
+        try {
+            console.log("something")
+            res.send("WELCOME TO TODOS")
+        } catch (error) {
+            next(error);
+        }
+    }
+
+)
+
+
+//route not found error {last of the all routes }
+app.use((req: Request, res: Response , next: NextFunction) => {
+    res.status(400).json({ message: "Route not find" })
+})
+
+
+// global error handling - it's must be last of the all routes
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    if (error) {
+        console.log("something ERROR")
+        res.status(500).json({ message: "Something wrong", error })
+    }
 })
 
 
